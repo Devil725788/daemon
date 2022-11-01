6439bc74973aa5f5cebec3b05491429c642aa755
Devil725788 <36689602+Devil725788@users.noreply.github.com>
2022-11-01 18:59:52 +0530

    Pull 8
    -Fixed Flash Light
    -Fixed PlayerJump
    -Added Hit Impact
    -Added Impact in Gun Script
    -Added hit physics
    -Added Enemy Destory Script

--- daemon/Assets/Scripts/Gun.cs
+++ daemon/Assets/Scripts/Gun.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public float damage = 10f;
+    public float damage = 10f; //Bullet Damage
 
-    public float range = 100f;
+    public float range = 100f; //Range of Bullet
+
+    public float fireRate = 15f; //Fire rate
+
+    public float impactForce = 30f; //Force of impact
 
     public Camera fpsCam;
 
     public ParticleSystem muzzleFlash;
 
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (
+            Input.GetButtonDown("Fire1") && /* replace GetButton with GetButtonDown to change into tap mode*/
+            Time.time >= nextTimeToFire
+        )
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -38,6 +50,17 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage (damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO =
+                Instantiate(impactEffect,
+                hit.point,
+                Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
